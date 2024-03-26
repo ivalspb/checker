60d059ba28998c2d0f51d6bc8fc6c71e52a16de7
ivalspb <ival.spb@gmail.com>
2024-03-26 07:53:19 +0300

    Добавьте файлы проекта.

---
+++ brakes_checker/brakes_checker.cpp
@@ -0,0 +1,38 @@
+﻿// brakes_checker.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stack>
+#include <string>
+
+bool check_brakes(std::string s)
+{
+    std::stack<char> st;
+    for (const char simb : s)
+    {
+        if (simb == '(' || simb == '[' || simb == '{') st.push(simb);
+        else
+        {
+            if (st.empty()) return false;
+            switch (simb)
+            {
+            case ')':
+                if (st.top() != '(') return false;
+                break;
+            case ']':
+                if (st.top() != '[') return false;
+                break;
+            case '}':
+                if (st.top() != '{') return false;
+            }
+            st.pop();
+        }
+    }
+    return st.empty();
+
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ islands_counter/islands_counter.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+
+struct point
+{
+    size_t x, y;
+};
+
+size_t islands_counter(std::vector<std::vector<int>> lake)
+{
+    std::stack<point> islands_points;
+    size_t islands_counct = 0;
+    for(size_t i=0;i<lake.size();i++)
+        for (size_t j = 0; j < lake[i].size(); j++)
+        {
+            if (lake[i][j]) islands_points.push({ i,j });
+        }
+    while (!islands_points.empty())
+    {
+        point cur_point = islands_points.top();
+        if ((cur_point.x && lake[cur_point.x - 1][cur_point.y]) ||
+            (cur_point.y && lake[cur_point.x][cur_point.y - 1]) ||
+            (cur_point.x < (lake.size() - 1) && lake[cur_point.x + 1][cur_point.y]) ||
+            (cur_point.y < (lake[cur_point.x].size() - 1) && lake[cur_point.x][cur_point.y + 1]))
+        {
+            islands_points.pop();
+            lake[cur_point.x][cur_point.y] = 0;
+        }
+        else 
+        {
+            islands_counct++;
+            islands_points.pop();
+        }
+
+    }
+    return islands_counct;
+}
+
+int main()
+{
+    std::vector < std::vector<int >> lake = {
+       {0,0,0,0},
+       {0,0,0,0},
+       {0,0,0,1},
+       {0,0,1,1},
+    };
+    std::cout << islands_counter(lake);
+    return 0;
+
+}
