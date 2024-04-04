88768372a366591b9d0c0df37e78a3ec05ae9e28
ivalspb <ival.spb@gmail.com>
2024-04-04 18:42:14 +0300

    sum simple alg

--- find_sum/find_sum.cpp
+++ find_sum/find_sum.cpp
@@ -9,14 +9,19 @@
 
 int main()
 {
-    const size_t arr_size = 50;
+    const size_t arr_size = 20;
     std::array<int, arr_size> ar;
+    srand(time(NULL));
     for (size_t i = 0; i < arr_size; i++) ar[i] =rand() % 200 - 100;
     std::sort(ar.begin(),ar.end());
     for(int i:ar) std::cout <<i<< " ";
     
     int K = rand() % 200 - 100;
-    std::cout << std::endl << "K = " << K;
+    std::cout << std::endl << "K = " << K<<std::endl;
+
+    std::vector<int> res = find_sum_term(ar, K);
+    if (res.empty()) std::cout << "\nThere are not members at array as terms of " << K << std::endl;
+    else std::cout << res[0] << "+" << res[1] << "=" << K;
 
 }
 
--- find_sum/finder.h
+++ find_sum/finder.h
@@ -1,68 +1,23 @@
 #pragma once
 #include <array>
+#include <vector>
 
 template <typename T, size_t SIZE>
-size_t find_index_nearest_up(const std::array<T, SIZE>& ar, const int& val)
+std::vector<T> find_sum_term(const std::array<T, SIZE>& ar, const T& sum)
 {
-    if (!SIZE) return 0;
-    if (val < ar[0]||SIZE==1) return 0;
-    if (val>ar[SIZE - 1]) return SIZE-1;
-    size_t begin=0,end = SIZE-1;
-    while (ar[end]!=val)
-    {
-        if (ar[end] > val && ar[end - 1] < val) break;
-        if (ar[(end - begin) / 2] > val) end = (end - begin) / 2;
-        else begin = (end-begin) / 2;
-    }
-    return end;
-}
-
-template <typename T, size_t SIZE>
-std::pair<T, T> find_sum_term(const std::array<T, SIZE>& ar, const T& sum)
-{
-    //найти ближайшее к сумме число и к нулю в массиве и пытаться складывать, двигаясь вокруг этих чисел
-    size_t term1_i = find_nearest_up(sum), term0_i = find_nearest_up(0);
-    T delta_cur{}, delta = abs(ar[term1_i] + ar[term0_i] - sum);
-    if (!delta) return { ar[term1_i],ar[term0_i] };
-
-    int term0_up_lim, term0_bottom_lim, term1_up_lim, term1_bottom_lim,term0_delta,term1_delta;
-    for(bool out_direction = true;out_direction;out_direction--)
-    {
-        if (sum > 0 && out_direction)//0....term0_i...term1_i....SIZE - 1
-        {                            //  <<<                 >>>>
-            term0_bottom_lim = term0_i;
-            term0_up_lim = -1;
-            term1_up_lim = SIZE;
-            term1_bottom_lim = term1_i;
-            term0_delta = 1;
-            term1_delta = -1;
-        }
-        else if(sum<=0 && out_direction)//0....term1_i...term0_i....SIZE - 1
-        {                               //  <<<                 >>>>
-            term0_bottom_lim = term0_i;
-            term0_up_lim = SIZE - 1;
-            term1_bottom_lim = 0;
-            term1_up_lim = term1_i;
-        }
-        else if (sum > 0 && !out_direction)//0....term0_i..........term1_i....SIZE - 1
-        {                                  //            >>>     <<<
-            term0_bottom_lim = term0_i;
-            term0_up_lim = (term1_i - term0_i)/2+term0_i;
-            term1_bottom_lim = (term1_i - term0_i) / 2 + term0_i;
-            term1_up_lim = term1_i;
-        }
-        else
-        {                                   //0....term1_i..........term0_i....SIZE - 1        
-            term0_up_lim = term0_i;         //            >>>     <<<
-            term0_bottom_lim = (term1_i - term0_i) / 2 + term0_i;
-            term1_up_lim = (term1_i - term0_i) / 2 + term0_i;
-            term1_bottom_lim = term1_i;
-        }
-        //так как массив упорядочен, двигаемся либо наружу либо внутрь, пока дельта меньше
-        //пробуем двигаться сначала одним, потом другим
-        for (int i = term1_bottom_lim, j = term0_bottom_lim; i != term1_up_lim && j != term0_up_lim;i+term1_delta,j+term0_delta)
-        {
-
-        }
-    }
+	std::vector<T> res;
+	for (size_t i = 0, j = SIZE - 1; i < j;)
+	{
+		if (ar[i] + ar[j] == sum)
+		{
+			res.push_back(ar[i]);
+			res.push_back(ar[j]);
+			break;
+		}
+		else if (ar[i] + ar[j] > sum)
+			j--;
+		else
+			i++;
+	}
+	return res;
 }
